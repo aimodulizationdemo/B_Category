6d9c802e02d9125da1b5ef9d1ab99a8af581dc0e
Master <>
2019-02-21 15:41:56 +0800

    0.1

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by aizexin on 2019/2/21.
+//  Copyright © 2019 master. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator(B)
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+@end
+
+NS_ASSUME_NONNULL_END
